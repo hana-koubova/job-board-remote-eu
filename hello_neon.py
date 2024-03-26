59f585dfd422c07fb7f8a493d80718007ac6e61b
hana-koubova <122526703+hana-koubova@users.noreply.github.com>
2024-03-26 20:16:56 +0100

    Delete hello_neon.py

--- hello_neon.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-conn_str = "postgresql://neondb_owner:4slSQ8XqNYxm@ep-sweet-mode-a2vbydq9.eu-central-1.aws.neon.tech/neondb?sslmode=require"
-
-conn = psycopg2.connect(conn_str)
-
-with conn.cursor() as cur:
- cur.execute("SELECT 'hello neon';")
- print(cur.fetchall())
